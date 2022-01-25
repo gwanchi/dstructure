dc935bb6a2dea9e46476ecf3dc20274f3b7f4cd1
Erick Chrysostom <gwanchi@gmail.com>
2022-01-25 18:14:26 +0300

    Added area of the largest rectangle in the histogram

--- largest_rectangle_histogram.py
+++ largest_rectangle_histogram.py
@@ -47,3 +47,16 @@ def largest_rectangle3(heights):
     for i in range(1, len(heights)-1):
         max_area=max(max_area, heights[i]*(from_right[i]-from_left[i]-1))
     return max_area
+
+def largest_rectangle4(heights):
+    heights=[-1]+heights+[-1]
+    max_area=0
+    stack=[(0,-1)]
+    for i in range(1, len(heights)):
+        start=i
+        while stack[-1][1] > heights[i]:
+            top_index, top_height = stack.pop()
+            max_area = max(max_area, top_height*(i-top_index))
+            start=top_index
+        stack.append((start, heights[i]))
+    return max_area
